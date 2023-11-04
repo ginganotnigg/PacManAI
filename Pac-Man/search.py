33221d82ce58c3002b4d4048bf1133b289ddde33
bhdang311003 <114203625+bhdang311003@users.noreply.github.com>
2023-11-04 17:50:38 +0700

    Update search.py
    add A*

--- Pac-Man/search.py
+++ Pac-Man/search.py
@@ -44,4 +44,49 @@ def DFS(startPos, endPos, boards):
     visited = []
     path = []
     return DFS_implement(path, visited, boards, startPos, endPos)
-    
+
+#A_start
+def min_list(a):
+    minn = a[0]
+    for i in a:
+        if i < minn:
+            minn = i
+    return minn
+
+def A_star(startPos, endPos, board):
+    f_array = []
+    for i in range(len(board)):
+        row = []
+        for j in range(len(board[0])):
+            if board[i][j] == 0 or board[i][j] == 2 or board[i][j] == 3:
+                h = float((int(end[0]) - i)**2 + (int(end[1]) - j)**2)**0.5
+                g = abs(int(start[0]) - i) + abs(int(start[1]) - j)
+                row.append(h + g)
+            else:
+                row.append(9999)
+        f_array.append(row)
+    openn = [startPos]
+    close = []
+    prev={}
+    while openn != []:
+        list = []
+        for i in openn:
+            list.append(f_array[int(i[0])][int(i[1])])
+        p = ''
+        for i in openn:
+            if f_array[int(i[0])][int(i[1])] == min_list(list):
+                p = i
+                break
+        openn.remove(p)
+        close.append(p)
+        if p == end:
+            break
+        for i in nextPos(p):
+            if i not in openn and i not in close:
+                openn.append(i)
+                prev[i] = p
+    path = [end]
+    while start not in path:
+        path.append(prev[path[len(path)-1]])
+    path.reverse()
+    return path
